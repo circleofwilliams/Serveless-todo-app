29b7592194f711efd33f50dd59d87f4c51b0f9d0
Olamide Williams <circleofwilliams@gmail.com>
2023-02-15 00:58:14 +0100

    Upating the serverless.yml file and adding authentication credentials

--- client/src/config.ts
+++ client/src/config.ts
@@ -5,7 +5,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-c6m821etj71b5p7l.us.auth0.com',            // Auth0 domain
+  clientId: 'Tp5AKDMp59wsqcibzS8zcJYE0hFNG2yu',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
